bbfaf963133506d3797f9b221e089c377b9d15af
DJAE <djaeardi80@gmail.com>
2024-11-28 11:32:34 +0100

    Modif et Supression

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import PythonHackingPortfolio from './PythonHackingPortfolio';
 import reportWebVitals from './reportWebVitals';
+
+// Importez Tailwind CSS
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PythonHackingPortfolio />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optionnel : si vous voulez mesurer les performances
 reportWebVitals();
